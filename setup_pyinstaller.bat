63def19752aad0e65bfa14c44423ec6a294cc4f3
vitae.sys <voldor0@gmail.com>
2024-11-05 21:15:35 +0200

    Updated setup scripts to include readme files

--- setup_pyinstaller.bat
+++ setup_pyinstaller.bat
@@ -1,2 +1,4 @@
-pyinstaller main.py --onefile --add-data "assets;assets" --name chess_debug
-pyinstaller main.py --onefile --windowed --add-data "assets;assets" --name chess
+pyinstaller main.py --onefile --add-data "assets;assets" --name chessvarc
+pyinstaller main.py --onefile --windowed --add-data "assets;assets" --name chessvarw
+copy cwda_2024.txt dist\cwda_2024.txt
+copy readme.txt dist\readme.txt
