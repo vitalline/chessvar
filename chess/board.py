7318a0797dfa0355d5bda2a5e7a15b133f43546f
vitae.sys <voldor0@gmail.com>
2025-03-01 14:34:58 +0200

    New pieces' total move count now persists between promotions, and is set to 1 for drops, if they are specified without metadata

--- chess/board.py
+++ chess/board.py
@@ -1505,12 +1505,7 @@ class Board(Window):
                         if royal_value and royal_value not in {'+', '-'}:
                             continue
                         has_moved = i in {0, len(trimmed_set) - 1}
-                        if has_moved:
-                            piece = piece_type(self)
-                            piece.movement.set_moves(1)
-                            drops[piece_type] = {pos: [piece] for pos in drop_squares}
-                        else:
-                            drops[piece_type] = {pos: [piece_type] for pos in drop_squares}
+                        drops[piece_type] = {pos: [piece_type] for pos in drop_squares}
             self.drops[drop_side] = drops
 
     def reset_promotions(self, piece_sets: dict[Side, list[type[AbstractPiece]]] | None = None) -> None:
@@ -4920,6 +4915,8 @@ class Board(Window):
                     drop = drop.of(drop.side or self.turn_side).on(move.pos_to)
                 else:
                     drop = drop(board=self, board_pos=move.piece.board_pos, side=self.turn_side)
+                    if drop.movement is not None:
+                        drop.movement.set_moves(mv.total_moves if (mv := move.piece.movement) else 1)
                 move.set(promotion=drop, placed_piece=drop_type_list[0])
                 for i, piece in enumerate(self.captured_pieces[self.turn_side][::-1]):
                     if piece == move.placed_piece:
@@ -4977,6 +4974,8 @@ class Board(Window):
                 promotion = promotion.of(promotion.side or move.piece.side).on(move.pos_to)
             elif isinstance(promotion, type) and issubclass(promotion, AbstractPiece):
                 promotion = promotion(board=self, board_pos=move.pos_to, side=move.piece.side)
+                if promotion.movement is not None:
+                    promotion.movement.set_moves(mv.total_moves if (mv := move.piece.movement) else 1)
             promoted_from = promotion.promoted_from or move.piece.promoted_from
             if not isinstance(move.piece, NoPiece):
                 promoted_from = promoted_from or type(move.piece)
@@ -5021,6 +5020,8 @@ class Board(Window):
                 piece = piece.of(piece.side or move.piece.side)
             else:
                 piece = piece(board=self, side=move.piece.side)
+                if piece.movement:
+                    piece.movement.set_moves(mv.total_moves if (mv := move.piece.movement) else 1)
             promoted_from = piece.promoted_from or move.piece.promoted_from
             if not isinstance(move.piece, NoPiece):
                 promoted_from = promoted_from or type(move.piece)
@@ -5090,6 +5091,8 @@ class Board(Window):
                 promotion = type(promotion)
             else:
                 promotion_piece = promotion(board=self, board_pos=pos, side=side)
+                if promotion_piece.movement:
+                    promotion_piece.movement.set_moves(mv.total_moves if (mv := piece.movement) else 1)
             if not self.edit_mode or (self.move_history and ((m := self.move_history[-1]) and m.is_edit != 1)):
                 promoted_from = promotion_piece.promoted_from or piece.promoted_from
                 if not isinstance(piece, NoPiece):
@@ -6031,6 +6034,8 @@ class Board(Window):
                                 piece = piece.of(piece.side or side).on(pos)
                             else:
                                 piece = piece(board=self, board_pos=move.pos_to, side=side)
+                                if piece.movement:
+                                    piece.movement.set_moves(mv.total_moves if (mv := move.piece.movement) else 1)
                             promoted_from = piece.promoted_from or move.piece.promoted_from
                             if not isinstance(move.piece, NoPiece):
                                 promoted_from = promoted_from or type(move.piece)
