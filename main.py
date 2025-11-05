d026727f656a253fcc089684ef5b273d14dbe1fc
vitae.sys <voldor0@gmail.com>
2025-11-05 13:43:28 +0200

    Disabled DPI scaling to fix rounding errors on app resize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from pyglet import options
+
 from chess.util import no_print
 
 with no_print():
@@ -11,4 +13,5 @@ from chess.board import Board
 if __name__ == '__main__':
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):  # noqa
         os.chdir(sys._MEIPASS)  # noqa
+    options.dpi_scaling = 'real'
     Board().run()
